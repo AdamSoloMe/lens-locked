176b6aceca1a860c764791e9aa71d3f850335d0a
adam_solomon_home <adamsolomon1801@gmail.com>
2025-05-04 23:18:18 -0400

    updated routing with Chi

--- main.go
+++ main.go
@@ -3,23 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
-//The net/http package in the standard library even has a type for it - http.HandlerFunc.
-
-//basic routing
-
-func (router Router) ServeHttp(w http.ResponseWriter, r *http.Request){
-	switch r.URL.Path{
-	case "/":
-		handlerFunc(w,r)
-	case "/contact":
-		contactHandler(w,r)
-	default:
-		http.Error(w,"Page Not found",http.StatusNotFound)
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","text/html: charset=utf-8")
+	fmt.Fprint(w,"<h1.Welcome to my awesome site!</h1>")
 }
-
 
 //default status code is 200
 //http request is not an interface but a pointer to a struct type of Http request
@@ -32,17 +23,38 @@ func handlerFunc(w http.ResponseWriter, r *http.Request){
 
 func contactHandler(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type","text/html; charset=utf-8")
-	fmt.Fprint(w,"<h1> Contact page</h1><p>To get in touch email me at</p>")
+	fmt.Fprint(w,"<h1> Contact page</h1><p>To get in touch email me at <a href=\"mailto:mrfanstatic2005@gmail.com\"></a> </p>")
 
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Contet-Type","text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1> FAQ Page </h1>
+	<ul>
+	<li><b> Is there a free version> </b> Yes! we offer a free trial for 30 days on any paid plans.</li>
+	<li><b> What are your support hours? </b> We have support staff answering emails on weekends 24/7,
+	though response time may be a bit slower on weekends.</li>
+	<li> <b> How do I contact support? </b> Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a></li>
+	</ul>
+	`)
 
 }
 
 
-
 func main(){
 	//how to register our handler function is with http.handlerfunc
-	http.HandleFunc("/",handlerFunc)//first pass in the pattern/paths that this function will handle then the function
-	http.HandleFunc("/contact",contactHandler)
+	// http.HandleFunc("/",handlerFunc)//first pass in the pattern/paths that this function will handle then the function
+	// http.HandleFunc("/contact",contactHandler)
+
+	//using chi 
+	r :=chi.NewRouter() //How to setup New Chi Router
+	r.Get("/",homeHandler)
+	r.Get("/contact",contactHandler)
+	r.Get("/faq",faqHandler)
+	r.NotFound(func (w http.ResponseWriter, r *http.Request){
+		http.Error(w,"Page not found",http.StatusNotFound)
+		
+	})
 
 	//good dev indicator that the server is running
 	fmt.Println("Starting Server on 3000")
