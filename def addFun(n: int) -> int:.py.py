7a74de041a67ac0a2395b0103ab78826aa958e2c
adam_solomon_home <adamsolomon1801@gmail.com>
2025-05-04 22:41:02 -0400

    inital commit with chi

---
+++ def addFun(n: int) -> int:.py.py
@@ -0,0 +1,9 @@
+def addFun(n: int) -> int:
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 2
+    return addFun(n - 1) + addFun(n - 2)
+
+def main():
+    print(addFun(6))
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//The net/http package in the standard library even has a type for it - http.HandlerFunc.
+
+
+//default status code is 200
+//http request is not an interface but a pointer to a struct type of Http request
+//the response writer is an interface allows for mulitpe impelemntaions and also to test it easier
+func handlerFunc(w http.ResponseWriter, r *http.Request){
+	//explictily setting content type
+	w.Header().Set("Content-Type","text/html; charset=utf-8")
+	fmt.Fprint(w,"<h1>welcome to my Awsome Site </h1>")//go print statement but allows to control where to print to
+}
+
+func contactHandler(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","text/html; charset=utf-8")
+	fmt.Fprint(w,"<h1> Contact page</h1><p>To get in touch email me at</p>")
+
+
+}
+
+
+
+func main(){
+	//how to register our handler function is with http.handlerfunc
+	http.HandleFunc("/",handlerFunc)//first pass in the pattern/paths that this function will handle then the function
+	http.HandleFunc("/contact",contactHandler)
+
+	//good dev indicator that the server is running
+	fmt.Println("Starting Server on 3000")
+	http.ListenAndServe(":3000",nil)//this function setups and starts the server to listern on port 3000
+
+	//routing deciding which pages to show the user 
+}
