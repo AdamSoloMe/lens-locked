646b3be761a2acabeb6e921b5cc08f32ca320c11
adam_solomon_home <adamsolomon1801@gmail.com>
2025-05-09 19:38:59 -0400

    rewrote all page logic to be using go html templates

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request){
 func contactHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type","text/html; charset=utf-8")
 	//how to avoid filepath issues 
-	tplPath :=filepath.Join("templates","contact.gohtml")
+	tplPath :=filepath.Join("templates","contacts.gohtml")
 	tpl, err := template.ParseFiles(tplPath)
 	if err != nil{ //placeholder for when I enventually put in parsing errors
 		log.Printf("Parising template: %v",err)
@@ -65,14 +65,20 @@ func contactHandler(w http.ResponseWriter, r *http.Request){
 
 func faqHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Contet-Type","text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1> FAQ Page </h1>
-	<ul>
-	<li><b> Is there a free version> </b> Yes! we offer a free trial for 30 days on any paid plans.</li>
-	<li><b> What are your support hours? </b> We have support staff answering emails on weekends 24/7,
-	though response time may be a bit slower on weekends.</li>
-	<li> <b> How do I contact support? </b> Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a></li>
-	</ul>
-	`)
+	//how to avoid filepath issues 
+	tplPath :=filepath.Join("templates","faq.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil{ //placeholder for when I enventually put in parsing errors
+		log.Printf("Parising template: %v",err)
+		http.Error(w,"There was an issue parsing this template", http.StatusInternalServerError)
+		return
+	}
+	err= tpl.Execute(w,nil)
+	if err !=nil{
+		log.Printf("executing template: %v",err)
+		http.Error(w,"There was an error executing the template ", http.StatusInternalServerError)
+		return
+	}
 
 }
 
